06a7767529d93d4338828e3095aa70aca5f9a75d
PedroAugustoPucMG <pedro.augusto.gda@gmail.com>
2026-09-03 20:26:02 -0300

    chore: configure production API endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use((config) => {
@@ -16,7 +16,6 @@ api.interceptors.request.use((config) => {
 
 api.interceptors.response.use(
   (response) => response,
-
   (error) => {
     const isLoginRequest =
       error.config?.url === "/auth/login";
